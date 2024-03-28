e36c6115a1424bf32e487447b5bd3f6ebda24e9f
muralisamui <muralisamui997@gmail.com>
2024-03-29 03:20:50 +0530

    set up lANDING page and loader

---
+++ src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,12 @@
+import ResponsiveAppBar from '../../components/NavBar/ResponsiveAppBar'
+import { Box } from '@mui/material'
+
+function LandingPage() {
+    return (
+        <Box sx={{ width: '100%' }}>
+            <ResponsiveAppBar />
+        </Box>
+    )
+}
+
+export default LandingPage
